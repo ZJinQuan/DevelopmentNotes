4b65541a9ef87dedc4af53d61b2f9334ffcd9ff3
ZZJQ <zhujinquan@aliyun.com>
2016-05-28 19:44:09 +0800

    图片处理

---
+++ DevelopmentNotes/DevelopmentNotes/图片处理/ImageTreated.h
@@ -0,0 +1,13 @@
+//
+//  ImageTreated.h
+//  DevelopmentNotes
+//
+//  Created by QUAN on 16/5/28.
+//  Copyright © 2016年 QUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageTreated : UIView
+
+@end
---
+++ DevelopmentNotes/DevelopmentNotes/图片处理/ImageTreated.m
@@ -0,0 +1,119 @@
+//
+//  ImageTreated.m
+//  DevelopmentNotes
+//
+//  Created by QUAN on 16/5/28.
+//  Copyright © 2016年 QUAN. All rights reserved.
+//
+
+#import "ImageTreated.h"
+
+@implementation ImageTreated
+
+
+/*
+ 
+ 根据image的frame来设置视图的显示模式, 处理图片的拉伸问题
+ 
+ */
+
+-(UIImage *)thumbnailWithImage:(UIImage *)image size:(CGSize)asize
+
+{
+    
+    UIImage *newimage;
+    
+    
+    if (nil == image) {
+        
+        newimage = nil;
+        
+    }
+    
+    else{
+        
+        UIGraphicsBeginImageContext(asize);
+        
+        [image drawInRect:CGRectMake(0, 0, asize.width, asize.height)];
+        
+        newimage = UIGraphicsGetImageFromCurrentImageContext();
+        
+        UIGraphicsEndImageContext();
+        
+    }
+    
+    return newimage;
+    
+}
+
+
+
+-(UIImage *)thumbnailWithImageWithoutScale:(UIImage *)image size:(CGSize)asize
+
+{
+    return image;
+    
+    UIImage *newimage;
+    
+    
+    if (nil == image) {
+        
+        newimage = nil;
+        
+    }
+    
+    else{
+        
+        CGSize oldsize = image.size;
+        
+        CGRect rect;
+        
+        if (asize.width/asize.height > oldsize.width/oldsize.height) {
+            
+            rect.size.width = asize.height*oldsize.width/oldsize.height;
+            
+            rect.size.height = asize.height;
+            
+            rect.origin.x = (asize.width - rect.size.width)/2;
+            
+            rect.origin.y = 0;
+            
+        }
+        
+        else{
+            
+            rect.size.width = asize.width;
+            
+            rect.size.height = asize.width*oldsize.height/oldsize.width;
+            
+            rect.origin.x = 0;
+            
+            rect.origin.y = (asize.height - rect.size.height)/2;
+            
+        }
+        
+        rect.size.width=asize.width;
+        rect.size.height=(oldsize.height*asize.width)/oldsize.width;
+        
+        UIGraphicsBeginImageContext(asize);
+        
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        
+        CGContextSetFillColorWithColor(context, [[UIColor clearColor] CGColor]);
+        
+        UIRectFill(CGRectMake(0, 0, asize.width, asize.height));
+        
+        [image drawInRect:rect];
+        
+        newimage = UIGraphicsGetImageFromCurrentImageContext();
+        
+        UIGraphicsEndImageContext();
+        
+    }
+    
+    
+    return newimage;
+    
+}
+
+@end
